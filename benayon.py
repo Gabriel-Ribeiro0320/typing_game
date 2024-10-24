cb573e60fa806dde3a2552cc4f669c81c1ffa2af
gabrielbenayon16 <gadecb.snf24@uea.edu.br>
2024-10-24 19:10:03 -0400

    arrow draw for left hand

--- benayon.py
+++ benayon.py
@@ -14,8 +14,10 @@ pygame.display.set_caption("TYPING GAME")
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
 # hands images
+
 left_hand_image = pygame.image.load("assets/left_hand.png")
 right_hand_image = pygame.image.load("assets/right_hand.png")
 left_hand_image = pygame.transform.scale(left_hand_image, (150, 150))
@@ -23,12 +25,44 @@ right_hand_image = pygame.transform.scale(right_hand_image, (150, 150))
 left_hand_pos = (100, 425)
 right_hand_pos = (550, 425)
 
-# arrow images
+# arrow image (red arrow for typing indication)
 
 arrow_image = pygame.image.load("assets/arrow.png")
 arrow_image = pygame.transform.scale(arrow_image, (50, 50))
-arrow_pos = (50, 50)
 
+# define arrow positions for each finger (left hand and right hand)
+
+arrow_positions = {
+    "left_pinky": (42, 417),
+    "left_ring": (65, 385),
+    "left_middle": (87, 370),
+    "left_index": (123, 370),
+    "left_thumb": (0, 0),
+    "right_index": (500, 400),
+    "right_middle": (560, 400),
+    "right_ring": (620, 400),
+    "right_pinky": (680, 400),
+    "right_thumb": (0, 0),
+}
+
+# map keys to respective fingers (touch typing)
+
+key_to_finger = {
+
+    # left hand keys
+
+    pygame.K_q: "left_pinky", pygame.K_a: "left_pinky", pygame.K_z: "left_pinky",
+    pygame.K_w: "left_ring", pygame.K_s: "left_ring", pygame.K_x: "left_ring",
+    pygame.K_e: "left_middle", pygame.K_d: "left_middle", pygame.K_c: "left_middle",
+    pygame.K_r: "left_index", pygame.K_f: "left_index", pygame.K_v: "left_index",
+
+    # right hand keys
+
+    pygame.K_u: "right_index", pygame.K_j: "right_index", pygame.K_m: "right_index",
+    pygame.K_i: "right_middle", pygame.K_k: "right_middle",
+    pygame.K_o: "right_ring", pygame.K_l: "right_ring",
+    pygame.K_p: "right_pinky", pygame.K_SEMICOLON: "right_pinky",
+}
 
 # game state
 
@@ -46,6 +80,10 @@ font1 = pygame.font.SysFont(None, 60)
 font2 = pygame.font.SysFont(None, 20)
 font3 = pygame.font.SysFont(None, 40)
 
+# to store current arrow position based on the key press
+
+current_arrow_pos = None
+
 def initial_menu_function():
     screen.fill(BLACK)
     text1 = font1.render("TYPING GAME", True, WHITE)
@@ -59,8 +97,16 @@ def game_function():
     screen.fill(BLACK)
     text1 = font2.render("PRESS ESC TO QUIT", True, WHITE)
     screen.blit(text1, (15, 10))
+
+    # display hands images
     screen.blit(left_hand_image, left_hand_pos)
     screen.blit(right_hand_image, right_hand_pos)
+
+    # if an arrow should be displayed, blit it on the correct finger
+
+    if current_arrow_pos:
+        screen.blit(arrow_image, current_arrow_pos)
+
     pygame.display.flip()
 
 
@@ -77,6 +123,7 @@ def end_menu_function():
 
 running = True
 while running:
+
     # verify events
 
     for event in pygame.event.get():
@@ -89,9 +136,16 @@ while running:
             elif game_status == game:
                 if event.key == pygame.K_ESCAPE:
                     game_status = end_menu
+                else:
+                    # check if the pressed key corresponds to a finger
+                    finger = key_to_finger.get(event.key)
+                    if finger:
+                        current_arrow_pos = arrow_positions[finger]  # update arrow position
+
             elif game_status == end_menu:
                 if event.key == pygame.K_r:
                     game_status = initial_menu
+                    current_arrow_pos = None  # reset arrow position
                 elif event.key == pygame.K_q:
                     running = False
 
