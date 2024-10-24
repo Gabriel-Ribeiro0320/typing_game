0a18805277d38925867cd0a370aae97e230ebc34
gabrielbenayon16 <gadecb.snf24@uea.edu.br>
2024-10-24 14:56:47 -0400

    assets added

--- benayon.py
+++ benayon.py
@@ -8,7 +8,7 @@ pygame.init()
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Typing Game")
+pygame.display.set_caption("TYPING GAME")
 
 # colors
 
