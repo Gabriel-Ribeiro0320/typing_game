a11371bd055c8623d1469659ca5f64627e6ac821
gabrielbenayon16 <gadecb.snf24@uea.edu.br>
2024-12-09 15:44:56 -0400

    word verification and sounds done

--- ui.py
+++ ui.py
@@ -7,39 +7,30 @@ class UI:
         self.can_spin = True
         self.current_odd = 1
         self.total_score = 0
+        self.roulette_sound = pygame.mixer.Sound("assets/audios/roulette_sound.mp3")
 
     def handle_event(self, event, slot_machine):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                slot_machine.spin()
-                self.current_odd = slot_machine.odd
-                self.update_total_score(slot_machine.odd)
-                self.can_spin = False
-                highlighted_index = slot_machine.highlighted_word_index
-                symbol_index = slot_machine.current_slots[highlighted_index]
-                selected_word = slot_machine.symbols[symbol_index]
+                if self.can_spin:
+                    highlighted_index = slot_machine.highlighted_word_index
+                    symbol_index = slot_machine.current_slots[highlighted_index]
+                    selected_word = slot_machine.symbols[symbol_index]
 
-                print(f"Índice destacado: {highlighted_index}")
-                print(f"Índice do símbolo: {symbol_index}")
-                print(f"Palavra destacada: {selected_word}")
-
-                if self.user_input.strip() == selected_word:
-                    slot_machine.spin()
-                    self.user_input = ""
-                    self.can_spin = True
-                else:
-                    print("Palavra incorreta!")
-                    self.can_spin = False
+                    if self.user_input.strip() == selected_word:
+                        self.roulette_sound.play()
+                        self.user_input = ""
+                        self.can_spin = True
+                        slot_machine.spin()
+                        self.current_odd = slot_machine.odd
+                        self.update_total_score(slot_machine.odd)
+                    else:
+                        self.can_spin = False
             elif event.key == pygame.K_BACKSPACE:
                 self.user_input = self.user_input[:-1]
-            elif event.key == pygame.K_RETURN:
-                self.check_user_input()
             else:
                 if event.unicode.isprintable():
                     self.user_input += event.unicode
-
-    def check_user_input(self):
-        print(f"Entrada do usuário: {self.user_input}")
 
     def draw_input_box(self, screen):
         input_text_surface = self.font.render(self.user_input, True, (255, 255, 255))
@@ -65,3 +56,8 @@ class UI:
         total_score_text = f"TOTAL: {self.total_score}"
         total_score_surface = self.font.render(total_score_text, True, (255, 255, 255))
         screen.blit(total_score_surface, (370, 707))
+
+    def play_roulette_sound(self):
+        """Reproduz o som da roleta."""
+        self.roulette_sound.play()
+
